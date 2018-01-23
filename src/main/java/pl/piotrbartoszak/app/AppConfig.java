afdd2b3dc6c2c90c1e18881474063aaf70530061
Piotr <p_bartoszak@wp.pl>
2018-01-23 11:06:11 +0100

    Configured project

---
+++ src/main/java/pl/piotrbartoszak/app/AppConfig.java
@@ -0,0 +1,9 @@
+package pl.piotrbartoszak.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"pl.piotrbartoszak"})
+public class AppConfig {
+}
---
+++ src/main/java/pl/piotrbartoszak/controller/TestController.java
@@ -0,0 +1,17 @@
+package pl.piotrbartoszak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping("")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
